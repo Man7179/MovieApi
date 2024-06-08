13bb65bf81182fc4d5eb8df93d8df661d60d99e4
Manoj Manchikatla <123497531+Man7179@users.noreply.github.com>
2024-06-09 00:51:36 +0530

    Add files via upload

---
+++ Backend/src/main/java/dev/Manoj/Movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.Manoj.Movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
